3878868eafd34837d19c4b971ed3b5ca43ef86b7
Dikachi-Dev <onyex11@gmail.com>
2024-03-24 05:57:39 +0100

    ReadMe

---
+++ README.md
@@ -0,0 +1,9 @@
+# File with Heading
+
+Sorting
+
+Failed this in a code test , had to comeback and make solve 😒😒
+
+Possibly to wipe my shame 🤦‍♂️
+
+
--- obj/Debug/net8.0/Sorting.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sorting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sorting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1df8495ec54c38a0c2470850ebac9c9b8de107d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sorting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sorting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
